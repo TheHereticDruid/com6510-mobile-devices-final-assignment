40fe85bbf9f62ed8053fd1390abe053f8bb4543d
Mangesh Hambarde <mhambarde1@sheffield.ac.uk>
2018-12-19 16:28:42 +0000

    First working app

---
+++ app/src/main/java/com6510/dcs/shef/ac/uk/BrowseAdapter.java
@@ -0,0 +1,91 @@
+package com6510.dcs.shef.ac.uk;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.util.List;
+
+import com6510.dcs.shef.ac.uk.gallery.R;
+
+public class BrowseAdapter extends RecyclerView.Adapter<BrowseAdapter.View_Holder> {
+    static private Context context;
+    private static List<ImageElement> items;
+
+    public BrowseAdapter(List<ImageElement> items) {
+        this.items = items;
+    }
+
+    public BrowseAdapter(Context cont, List<ImageElement> items) {
+        super();
+        this.items = items;
+        context = cont;
+    }
+
+    @Override
+    public View_Holder onCreateViewHolder(ViewGroup parent, int viewType) {
+        //Inflate the layout, initialize the View Holder
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.browse_item,
+                parent, false);
+        View_Holder holder = new View_Holder(v);
+        context= parent.getContext();
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(final View_Holder holder, final int position) {
+
+        //Use the provided View Holder on the onCreateViewHolder method to populate the
+        // current row on the RecyclerView
+        if (holder!=null && items.get(position)!=null) {
+            Bitmap myBitmap = BitmapFactory.decodeFile(items.get(position).file.getAbsolutePath());
+            holder.imageView.setImageBitmap(myBitmap);
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(context, ShowImageActivity.class);
+                    intent.putExtra("position", position);
+                    context.startActivity(intent);
+                }
+            });
+        }
+        //animate(holder);
+    }
+
+
+    // convenience method for getting data at click position
+    ImageElement getItem(int id) {
+        return items.get(id);
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public class View_Holder extends RecyclerView.ViewHolder  {
+        ImageView imageView;
+
+
+        View_Holder(View itemView) {
+            super(itemView);
+            imageView = (ImageView) itemView.findViewById(R.id.image_item);
+
+        }
+
+    }
+
+    public static List<ImageElement> getItems() {
+        return items;
+    }
+
+    public static void setItems(List<ImageElement> items) {
+        BrowseAdapter.items = items;
+    }
+}
---
+++ app/src/main/java/com6510/dcs/shef/ac/uk/ImageElement.java
@@ -0,0 +1,12 @@
+package com6510.dcs.shef.ac.uk;
+
+import java.io.File;
+
+public class ImageElement {
+    File file;
+
+    public ImageElement(File file) {
+        this.file = file;
+    }
+}
+
